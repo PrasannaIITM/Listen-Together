744777bbf5caf77a1b636aed0305f4c5ea5eabad
prasanna <prasanna.bartakke@gmail.com>
2021-06-27 19:19:46 +0530

    first commit

---
+++ music_controller/spotify/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import (
+    AuthURL,
+    PauseSong,
+    SkipSong,
+    spotify_callback,
+    IsAuthenticated,
+    CurrentSong,
+    PlaySong,
+)
+
+urlpatterns = [
+    path("get-auth-url/", AuthURL.as_view()),
+    path("redirect/", spotify_callback),
+    path("is-authenticated/", IsAuthenticated.as_view()),
+    path("current-song/", CurrentSong.as_view()),
+    path("pause/", PauseSong.as_view()),
+    path("play/", PlaySong.as_view()),
+    path("skip/", SkipSong.as_view()),
+]
