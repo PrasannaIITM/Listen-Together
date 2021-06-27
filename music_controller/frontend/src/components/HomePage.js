8ee12dc67aa003b2899f689acfd0f1023b58df1b
prasanna <prasanna.bartakke@gmail.com>
2021-06-27 21:18:14 +0530

    added info page

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
+import Info from './Info';
 import Room from './Room';
 import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect, } from "react-router-dom";
@@ -29,13 +30,16 @@ export default class HomePage extends Component{
             <Grid container spacing={3}>
                 <Grid item xs={12} align="center">
                     <Typography variant="h3" component="h3">
-                        House Party
+                        Listen Together
                     </Typography>
                 </Grid>
                 <Grid item xs={12} align="center">
                     <ButtonGroup disableElevation variant="contained" color="primary">
                         <Button color="primary" to="/join" component={Link}>
                             Join a Room
+                        </Button>
+                        <Button color="default" to="/info" component={Link}>
+                            Info
                         </Button>
                         <Button color="secondary" to="/create" component={Link}>
                             Create a Room
@@ -59,6 +63,8 @@ export default class HomePage extends Component{
                     return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`} />) : (this.renderHomePage())
                 }} />
                 <Route path ='/join' component={RoomJoinPage}>
+                </Route>
+                <Route path ='/info' component={Info}>
                 </Route>
                 <Route path ='/create' component={CreateRoomPage}>
                 </Route>
